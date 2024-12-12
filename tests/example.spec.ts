7f22e761a758d1d52d76749a35ca4cdc386a588c
rxbx <rxbx972@gmail.com>
2024-12-12 21:31:20 +0900

    weverse test - 로그인 완료 후 홈 복귀

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,40 @@
 import { test, expect } from '@playwright/test';
 
+const userEmail = ''; 
+const userPassword = '';
+
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://weverse.io/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Weverse/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('weverse test', async ({ page }) => {
+  await page.goto('https://weverse.io/');
+  console.log("위버스 페이지 진입");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const modalCloseButton = page.locator('button[class^="BaseModalView_bottom_button__"]').nth(0);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  const signInButton = page.locator('button[class^="HeaderView_link_sign__"]');
+  const signInButton2 = page.locator('button', { hasText: 'Sign in' });
+  const signInButton3 = page.getByRole('button', { name: 'Sign in'}); 
+  
+  await modalCloseButton.click();
+  await signInButton.click();
+
+  await expect(page).toHaveURL(/account.weverse/);
+  console.log("로그인 페이지 진입");
+
+  const userEmailInput = page.locator('input[name="userEmail"]');
+  const userPasswordInput = page.locator('input[name="password"]');
+
+  await userEmailInput.fill(userEmail);
+  await page.locator('button[type="submit"]').nth(1).click();
+
+  await userPasswordInput.fill(userPassword);
+  await page.locator('button[type="submit"]').click();
+  console.log("로그인 완료");
+
+  await expect(page).not.toHaveURL(/account.weverse/);
 });
